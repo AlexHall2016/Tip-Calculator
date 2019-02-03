ed1ec9a07b62c045b2bd534cc27fc46f959be65e
Alexander Hall <alexanderhall@Alexanders-iMac.local>
2019-02-03 13:21:45 -0800

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Alexander Hall on 2/3/19.
+//  Copyright © 2019 Alexander Hall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
